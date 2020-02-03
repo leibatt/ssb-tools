c60d4e737435d91b6a45cb9885da88556c19d5d2
Leilani Battle <leilani@cs.umd.edu>
2020-02-03 13:48:55 -0500

    adding more scripts from crossfilter benchmark

---
+++ setup/stop-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SOURCE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+for DBMS in $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && ls -d */ )
+do
+  if [ "$DBMS" != "verdictdb/" ]; then # verdictdb just uses postgresql
+    echo ${SOURCE}/${DBMS}./stop-database.sh
+    ${SOURCE}/${DBMS}./stop-database.sh
+  fi
+done
